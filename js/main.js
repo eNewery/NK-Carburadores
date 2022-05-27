7d582b7d614a182573ca21f5b36eaf31cabcc8d6
Ulises <robdiaz247@gmail.com>
2022-05-27 02:36:45 -0300

    alertfunction

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
-/* Acordeón */
+
 'use strict'
 
 const bloque    = document.querySelectorAll('.bloque')
 const h2        = document.querySelectorAll('.h2')
-    
+
+const comprar1 = document.getElementById("buy1");
+const comprar2 = document.getElementById("buy2");
+const comprar3 = document.getElementById("buy3");
+const comprar4 = document.getElementById("buy4");
+
+comprar1.onclick = erro;
+comprar2.onclick = erro;
+comprar3.onclick = erro;
+comprar4.onclick = erro;
 
 
+
+
+function erro() {
+  alert("¡Compra realizada con éxito!")
+}
+
+
+
+
+/* Acordeon */
 h2.forEach( ( cadaH2 , i )=>{
     h2[i].addEventListener('click', ()=>{
 
